bb2d342f727938cc87ee1c95ee6c5f650e244e71
tozevv <tozevv@gmail.com>
2014-05-26 09:30:16 +0100

    Added Mono support. Add lost files

---
+++ src/Rebus.Tests.Redis/Transports/Redis/RedisMessageQueueTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Rebus.Tests.Transports.Redis
+{
+	using System.Text;
+
+    using NUnit.Framework;
+    using Rebus.Transports.Redis;
+	using Rebus.Bus;
+	using StackExchange.Redis;
+
+    /// <summary>
+    /// Unit tests for Redis Message Queue.
+    /// </summary>
+    [TestFixture]
+    public class RedisMessageQueueTests
+    {
+		private RedisServer server = null;
+        [TestFixtureSetUp]
+        public void Init()
+        {
+			server = new RedisServer (6666);
+			server.Start ();
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+			server.Stop ();
+        }
+
+        [Test]
+		public void WhenSendingMessage_ThenMessageIsDelivered()
+        {
+			var transactionContext = new NoTransaction ();
+			string queueName = "sampleQueue";
+			string sentMessage = "message";
+			var configuration = server.ClientConfiguration;
+
+			var queue = new RedisMessageQueue(configuration, queueName);
+
+			queue.Send (queueName, CreateStringMessage(sentMessage), transactionContext);
+			var receivedMessage = GetStringMessage(queue.ReceiveMessage (transactionContext));
+
+			Assert.AreEqual (sentMessage, receivedMessage);
+        }
+
+		private TransportMessageToSend CreateStringMessage(string contents)
+		{
+			return
+				new TransportMessageToSend
+			{
+				Body = Encoding.UTF8.GetBytes(contents),
+				Label = typeof(string).FullName
+			};
+		}
+
+		private string GetStringMessage(ReceivedTransportMessage message)
+		{
+			return message == null ? null :
+				Encoding.UTF8.GetString (message.Body);
+		}
+
+
+    }
+}
---
+++ src/Rebus.Tests.Redis/Transports/Redis/RedisServer.cs
@@ -0,0 +1,86 @@
+﻿namespace Rebus.Tests.Transports.Redis
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+	using StackExchange.Redis;
+
+    /// <summary>
+    /// Controls redis server start, stop and failure.
+    /// </summary>
+    public class RedisServer
+    {
+		private const string redisWindowsPath = @"Redis-64.2.8.4";
+		private const string redisMacPath = @"Redis-64.2.8.9.macosx";
+        
+		private Process process; 
+		private readonly string serverCommand = null;
+		private readonly string clientCommand = null;
+
+		private readonly int port;
+
+		public RedisServer(int port)
+		{
+			this.port = port;
+			// assume unix is mac for now...
+			if (Environment.OSVersion.Platform == PlatformID.Unix || 
+				Environment.OSVersion.Platform == PlatformID.MacOSX) 
+			{
+				serverCommand = Path.Combine("..", "..", "..", "packages", redisMacPath, "redis-server");
+				clientCommand = Path.Combine("..", "..", "..", "packages", redisMacPath, "redis-cli");
+			} else 
+			{
+				serverCommand = Path.Combine("..", "..", "..", "packages", redisWindowsPath, "redis-server.exe");
+				clientCommand = Path.Combine("..", "..", "..", "packages", redisWindowsPath, "redis-cli.exe");
+			}
+			this.ClientConfiguration = new ConfigurationOptions() 
+			{
+				EndPoints =
+				{
+					{ "127.0.0.1", this.port },
+				},
+				KeepAlive = 180
+			};
+		}
+
+        public void Start() 
+        {
+			Stop (); // Confirm is not running...
+
+			System.Diagnostics.Debug.WriteLine ("Test");
+	
+			process = Process.Start(new ProcessStartInfo() {
+				FileName = serverCommand,
+				UseShellExecute = true,
+				Arguments = string.Format("--port {0}", port)
+			});
+
+			process.WaitForExit (500);
+
+			if (process.HasExited) 
+			{
+				throw new Exception ("Cannot start redis server");
+			}
+        }
+
+        public void Stop() 
+        {
+			Process.Start (clientCommand, string.Format ("-p {0} shutdown", port)).WaitForExit ();
+        }
+
+        public void Crash()
+        {
+            process.Kill();
+        }
+
+		public ConfigurationOptions ClientConfiguration 
+		{
+			get;
+			private set;
+		}
+    }
+}
