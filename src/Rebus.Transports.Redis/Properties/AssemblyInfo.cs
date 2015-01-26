c9b5b7da61f8dd05c457ec6fef9c3a89b806eba9
tozevv <tozevv@gmail.com>
2015-01-26 11:17:40 +0000

    Added support for nuget pack on travis.

--- src/Rebus.Transports.Redis/Properties/AssemblyInfo.cs
+++ src/Rebus.Transports.Redis/Properties/AssemblyInfo.cs
@@ -24,16 +24,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d79c6798-79c6-47d3-b167-a9b1b37a7ff1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0")]
 [assembly: AssemblyFileVersion("0.1.0")]
 [assembly: AssemblyInformationalVersion("0.1.0")]
